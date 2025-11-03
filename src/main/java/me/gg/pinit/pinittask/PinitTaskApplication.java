8fa4f0b055ab6a8dbf8303406791d6a172842179
GoGradually <chkun3109@gmail.com>
2025-11-03 20:22:52 +0900

    feat: 초기 Spring Boot 애플리케이션 설정 및 Gradle 구성 추가

---
+++ src/main/java/me/gg/pinit/pinittask/PinitTaskApplication.java
@@ -0,0 +1,13 @@
+package me.gg.pinit.pinittask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinitTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinitTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/gg/pinit/pinittask/PinitTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gg.pinit.pinittask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinitTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
