d937862c44e5236f6e9c3d55c8efb138d77d1452
GoGradually <chkun3109@gmail.com>
2025-11-06 00:54:13 +0900

    feat: 진행 중인 일정의 시작, 일시정지, 취소 및 완료 기능 추가

--- src/main/java/me/gg/pinit/pinittask/domain/schedule/model/InProgressState.java
+++ src/main/java/me/gg/pinit/pinittask/domain/schedule/model/InProgressState.java
@@ -1,5 +1,8 @@
 package me.gg.pinit.pinittask.domain.schedule.model;
 
+import me.gg.pinit.pinittask.domain.schedule.exception.IllegalTransitionException;
+import me.gg.pinit.pinittask.domain.schedule.vo.ScheduleHistory;
+
 import java.time.ZonedDateTime;
 
 public class InProgressState implements ScheduleState{
@@ -7,22 +10,28 @@ public class InProgressState implements ScheduleState{
 
     @Override
     public void start(Schedule ctx, ZonedDateTime startTime) {
-
+        throw new IllegalTransitionException("진행 중인 일정을 다시 시작할 수 없습니다.");
     }
 
     @Override
     public void suspend(Schedule ctx, ZonedDateTime suspendTime) {
-
+        ScheduleHistory history = ctx.getHistory();
+        ctx.updateHistoryTo(history.recordStop(suspendTime));
+        ctx.setState(new NotStartedState());
     }
 
     @Override
     public void cancel(Schedule ctx) {
-
+        ScheduleHistory history = ctx.getHistory();
+        ctx.updateHistoryTo(history.rollback());
+        ctx.setState(new NotStartedState());
     }
 
     @Override
     public void finish(Schedule ctx, ZonedDateTime finishTime) {
-
+        ScheduleHistory history = ctx.getHistory();
+        ctx.updateHistoryTo(history.recordStop(finishTime));
+        ctx.setState(new CompletedState());
     }
 
     @Override
