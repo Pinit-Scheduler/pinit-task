38cb988fb546adc63ab3aed8b3f2938cb24b401a
GoGradually <chkun3109@gmail.com>
2025-11-05 12:46:57 +0900

    feat: LocalTimeConverter 클래스 추가

---
+++ src/main/java/me/gg/pinit/pinittask/domain/member/service/LocalTimeConverter.java
@@ -0,0 +1,19 @@
+package me.gg.pinit.pinittask.domain.member.service;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.time.LocalTime;
+
+@Converter
+public class LocalTimeConverter implements AttributeConverter<LocalTime, String> {
+    @Override
+    public String convertToDatabaseColumn(LocalTime attribute) {
+        return attribute == null ? null : attribute.toString();
+    }
+
+    @Override
+    public LocalTime convertToEntityAttribute(String dbData) {
+        return dbData == null ? null : LocalTime.parse(dbData);
+    }
+}
