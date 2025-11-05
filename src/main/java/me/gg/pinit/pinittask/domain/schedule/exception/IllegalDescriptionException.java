b9414ebf6e2feeb3196f0ff13b98092f965c5d0a
GoGradually <chkun3109@gmail.com>
2025-11-05 20:11:49 +0900

    feat: IllegalDescriptionException 및 IllegalTitleException 클래스 추가

---
+++ src/main/java/me/gg/pinit/pinittask/domain/schedule/exception/IllegalDescriptionException.java
@@ -0,0 +1,7 @@
+package me.gg.pinit.pinittask.domain.schedule.exception;
+
+public class IllegalDescriptionException extends RuntimeException {
+    public IllegalDescriptionException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/me/gg/pinit/pinittask/domain/schedule/exception/IllegalTitleException.java
@@ -0,0 +1,7 @@
+package me.gg.pinit.pinittask.domain.schedule.exception;
+
+public class IllegalTitleException extends RuntimeException {
+    public IllegalTitleException(String message) {
+        super(message);
+    }
+}
