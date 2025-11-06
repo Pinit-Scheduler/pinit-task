a4e9eb6c480388f6b7f97e404dcff0b3820eb0a6
GoGradually <chkun3109@gmail.com>
2025-11-06 12:10:15 +0900

    test: NotStartedStateTest에서 ScheduleCompletedEvent에 대한 검증 추가

--- src/test/java/me/gg/pinit/pinittask/domain/schedule/model/NotStartedStateTest.java
+++ src/test/java/me/gg/pinit/pinittask/domain/schedule/model/NotStartedStateTest.java
@@ -2,10 +2,12 @@ package me.gg.pinit.pinittask.domain.schedule.model;
 
 import me.gg.pinit.pinittask.domain.events.DomainEvent;
 import me.gg.pinit.pinittask.domain.events.DomainEvents;
+import me.gg.pinit.pinittask.domain.schedule.event.ScheduleCompletedEvent;
 import me.gg.pinit.pinittask.domain.schedule.exception.IllegalTransitionException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
 import java.util.Deque;
 
 import static me.gg.pinit.pinittask.domain.schedule.model.ScheduleUtils.ENROLLED_TIME;
@@ -68,6 +70,13 @@ class NotStartedStateTest {
         Deque<DomainEvent> eventsAndClear = DomainEvents.getEventsAndClear();
         assertThat(eventsAndClear).hasSize(1);
         DomainEvent event = eventsAndClear.peek();
+
+        if (event instanceof ScheduleCompletedEvent completedEvent) {
+            assertThat(completedEvent.getDuration()).isEqualTo(Duration.ZERO);
+            assertThat(completedEvent.getOwnerId()).isEqualTo(schedule.getOwnerId());
+            assertThat(completedEvent.getTaskType()).isEqualTo(schedule.getTemporalConstraint().getTaskType());
+        }
+
         assertThat(event).isInstanceOf(me.gg.pinit.pinittask.domain.schedule.event.ScheduleCompletedEvent.class);
         assertThat(schedule.isCompleted()).isTrue();
     }
