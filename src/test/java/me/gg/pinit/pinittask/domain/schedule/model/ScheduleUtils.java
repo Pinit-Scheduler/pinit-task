be86bdbc493f93194d291532dbf504e50f790306
GoGradually <chkun3109@gmail.com>
2025-11-10 23:41:05 +0900

    feat: ScheduleUtils 클래스에 다양한 일정 생성 메서드 추가

--- src/test/java/me/gg/pinit/pinittask/domain/schedule/model/ScheduleUtils.java
+++ src/test/java/me/gg/pinit/pinittask/domain/schedule/model/ScheduleUtils.java
@@ -4,59 +4,18 @@ import me.gg.pinit.pinittask.domain.events.DomainEvents;
 import me.gg.pinit.pinittask.domain.schedule.vo.ImportanceConstraint;
 import me.gg.pinit.pinittask.domain.schedule.vo.TemporalConstraint;
 
+import java.lang.reflect.Field;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.ZonedDateTime;
 
 public class ScheduleUtils {
-    public static ZonedDateTime ENROLLED_TIME = ZonedDateTime.of(2025,
-            10,
-            1,
-            10,
-            0,
-            0,
-            0,
-            Clock.systemDefaultZone().getZone());
-    public static ZonedDateTime START_TIME = ZonedDateTime.of(2025,
-            10,
-            1,
-            11,
-            0,
-            0,
-            0,
-            Clock.systemDefaultZone().getZone());
-    public static ZonedDateTime SUSPEND_TIME = ZonedDateTime.of(2025,
-            10,
-            1,
-            12,
-            0,
-            0,
-            0,
-            Clock.systemDefaultZone().getZone());
-    public static ZonedDateTime RESTART_TIME = ZonedDateTime.of(2025,
-            10,
-            1,
-            13,
-            0,
-            0,
-            0,
-            Clock.systemDefaultZone().getZone());
-    public static ZonedDateTime FINISH_TIME = ZonedDateTime.of(2025,
-            10,
-            1,
-            15,
-            0,
-            0,
-            0,
-            Clock.systemDefaultZone().getZone());
-    public static ZonedDateTime DEADLINE_TIME = ZonedDateTime.of(2025,
-            10,
-            10,
-            0,
-            0,
-            0,
-            0,
-            Clock.systemDefaultZone().getZone());
+    public static ZonedDateTime ENROLLED_TIME = ZonedDateTime.of(2025, 10, 1, 10, 0, 0, 0, Clock.systemDefaultZone().getZone());
+    public static ZonedDateTime START_TIME = ZonedDateTime.of(2025, 10, 1, 11, 0, 0, 0, Clock.systemDefaultZone().getZone());
+    public static ZonedDateTime SUSPEND_TIME = ZonedDateTime.of(2025, 10, 1, 12, 0, 0, 0, Clock.systemDefaultZone().getZone());
+    public static ZonedDateTime RESTART_TIME = ZonedDateTime.of(2025, 10, 1, 13, 0, 0, 0, Clock.systemDefaultZone().getZone());
+    public static ZonedDateTime FINISH_TIME = ZonedDateTime.of(2025, 10, 1, 15, 0, 0, 0, Clock.systemDefaultZone().getZone());
+    public static ZonedDateTime DEADLINE_TIME = ZonedDateTime.of(2025, 10, 10, 0, 0, 0, 0, Clock.systemDefaultZone().getZone());
 
     public static ZonedDateTime TIME_1 = ENROLLED_TIME;
     public static ZonedDateTime TIME_2 = ENROLLED_TIME.plusDays(1);
@@ -79,62 +38,132 @@ public class ScheduleUtils {
     }
 
     public static Schedule getNotStartedSchedule() {
-        return new Schedule(
-                1L,
-                "Sample Schedule",
-                "sample description",
-                ENROLLED_TIME,
-                getTemporalConstraintSample(),
-                getImportanceConstraintSample());
+        return new Schedule(1L, "Sample Schedule", "sample description", ENROLLED_TIME, getTemporalConstraintSample(), getImportanceConstraintSample());
     }
 
-    public static Schedule getInProgressSchedule() {
-        Schedule schedule = new Schedule(
-                1L,
-                "Sample Schedule",
-                "sample description",
-                ENROLLED_TIME,
-                getTemporalConstraintSample(),
-                getImportanceConstraintSample());
+    public static Schedule getNotStartedSchedule(
+            Long id,
+            Long ownerId,
+            String title,
+            String description,
+            ZonedDateTime enrolledTime,
+            TemporalConstraint temporalConstraint,
+            ImportanceConstraint importanceConstraint
+    ) {
+        Schedule schedule = new Schedule(ownerId, title, description, enrolledTime, temporalConstraint, importanceConstraint);
+        setScheduleId(schedule, id);
+        return schedule;
+    }
+
+    public static Schedule getNotStartedSchedule(Long id) {
+        Schedule schedule = getNotStartedSchedule();
+        setScheduleId(schedule, id);
+        return schedule;
+    }
+
+    public static Schedule getInProgressSchedule(
+            Long id,
+            Long ownerId,
+            String title,
+            String description,
+            ZonedDateTime enrolledTime,
+            TemporalConstraint temporalConstraint,
+            ImportanceConstraint importanceConstraint
+    ) {
+        Schedule schedule = new Schedule(ownerId, title, description, enrolledTime, temporalConstraint, importanceConstraint);
+        setScheduleId(schedule, id);
         schedule.start(START_TIME);
         return schedule;
     }
 
-    public static Schedule getSuspendedSchedule() {
-        Schedule schedule = new Schedule(
-                1L,
-                "Sample Schedule",
-                "sample description",
-                ENROLLED_TIME,
-                getTemporalConstraintSample(),
-                getImportanceConstraintSample());
+
+    public static Schedule getInProgressSchedule() {
+        Schedule schedule = new Schedule(1L, "Sample Schedule", "sample description", ENROLLED_TIME, getTemporalConstraintSample(), getImportanceConstraintSample());
+        schedule.start(START_TIME);
+        return schedule;
+    }
+
+    public static Schedule getInProgressSchedule(Long id) {
+        Schedule schedule = getInProgressSchedule();
+        setScheduleId(schedule, id);
+        return schedule;
+    }
+
+    public static Schedule getSuspendedSchedule(
+            Long id,
+            Long ownerId,
+            String title,
+            String description,
+            ZonedDateTime enrolledTime,
+            TemporalConstraint temporalConstraint,
+            ImportanceConstraint importanceConstraint
+    ) {
+        Schedule schedule = new Schedule(ownerId, title, description, enrolledTime, temporalConstraint, importanceConstraint);
+        setScheduleId(schedule, id);
         schedule.start(START_TIME);
         schedule.suspend(SUSPEND_TIME);
         return schedule;
     }
 
-    public static Schedule getCompletedSchedule() {
-        Schedule schedule = new Schedule(
-                1L,
-                "Sample Schedule",
-                "sample description",
-                ENROLLED_TIME,
-                getTemporalConstraintSample(),
-                getImportanceConstraintSample());
+    public static Schedule getSuspendedSchedule(Long id) {
+        Schedule schedule = getSuspendedSchedule();
+        setScheduleId(schedule, id);
+        return schedule;
+    }
+
+    public static Schedule getSuspendedSchedule() {
+        Schedule schedule = new Schedule(1L, "Sample Schedule", "sample description", ENROLLED_TIME, getTemporalConstraintSample(), getImportanceConstraintSample());
+        schedule.start(START_TIME);
+        schedule.suspend(SUSPEND_TIME);
+        return schedule;
+    }
+
+    public static Schedule getCompletedSchedule(
+            Long id,
+            Long ownerId,
+            String title,
+            String description,
+            ZonedDateTime enrolledTime,
+            TemporalConstraint temporalConstraint,
+            ImportanceConstraint importanceConstraint
+    ) {
+        Schedule schedule = new Schedule(ownerId, title, description, enrolledTime, temporalConstraint, importanceConstraint);
+        setScheduleId(schedule, id);
         schedule.start(START_TIME);
         schedule.finish(FINISH_TIME);
         DomainEvents.getEventsAndClear();
         return schedule;
     }
 
-    public static TemporalConstraint getTemporalConstraintSample(){
-        return new TemporalConstraint(
-                DEADLINE_TIME,
-                Duration.ZERO,
-                TaskType.DEEP_WORK);
+    public static Schedule getCompletedSchedule() {
+        Schedule schedule = new Schedule(1L, "Sample Schedule", "sample description", ENROLLED_TIME, getTemporalConstraintSample(), getImportanceConstraintSample());
+        schedule.start(START_TIME);
+        schedule.finish(FINISH_TIME);
+        DomainEvents.getEventsAndClear();
+        return schedule;
     }
 
-    public static ImportanceConstraint getImportanceConstraintSample(){
+    public static Schedule getCompletedSchedule(Long id) {
+        Schedule schedule = getCompletedSchedule();
+        setScheduleId(schedule, id);
+        return schedule;
+    }
+
+    public static TemporalConstraint getTemporalConstraintSample() {
+        return new TemporalConstraint(DEADLINE_TIME, Duration.ZERO, TaskType.DEEP_WORK);
+    }
+
+    public static ImportanceConstraint getImportanceConstraintSample() {
         return new ImportanceConstraint(5, 5);
+    }
+
+    private static void setScheduleId(Schedule schedule, Long id) {
+        try {
+            Field idField = Schedule.class.getDeclaredField("id");
+            idField.setAccessible(true);
+            idField.set(schedule, id);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
