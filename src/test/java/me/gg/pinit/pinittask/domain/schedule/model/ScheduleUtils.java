518cf2980ed9bc0d0c6cc788dd7bd77887cff090
GoGradually <chkun3109@gmail.com>
2025-11-10 23:11:46 +0900

    feat: ScheduleUtils 클래스에 연결된 일정 샘플 생성 메서드 추가

--- src/test/java/me/gg/pinit/pinittask/domain/schedule/model/ScheduleUtils.java
+++ src/test/java/me/gg/pinit/pinittask/domain/schedule/model/ScheduleUtils.java
@@ -58,6 +58,26 @@ public class ScheduleUtils {
             0,
             Clock.systemDefaultZone().getZone());
 
+    public static ZonedDateTime TIME_1 = ENROLLED_TIME;
+    public static ZonedDateTime TIME_2 = ENROLLED_TIME.plusDays(1);
+    public static ZonedDateTime TIME_3 = TIME_2.plusDays(1);
+    public static ZonedDateTime TIME_4 = TIME_3.plusDays(1);
+    public static ZonedDateTime TIME_5 = TIME_4.plusDays(1);
+    public static ZonedDateTime TIME_6 = TIME_5.plusDays(1);
+
+    public static Schedule getConnectedScheduleSample() {
+        Schedule before1 = new Schedule(1L, "A", "A", TIME_1, new TemporalConstraint(TIME_4, Duration.ofHours(5), TaskType.DEEP_WORK), new ImportanceConstraint(5, 5));
+        Schedule before2 = new Schedule(1L, "B", "B", TIME_1, new TemporalConstraint(TIME_4, Duration.ofHours(5), TaskType.DEEP_WORK), new ImportanceConstraint(5, 5));
+        Schedule now = new Schedule(1L, "C", "C", TIME_2, new TemporalConstraint(TIME_5, Duration.ofHours(5), TaskType.DEEP_WORK), new ImportanceConstraint(5, 5));
+        Schedule after1 = new Schedule(1L, "D", "D", TIME_3, new TemporalConstraint(TIME_6, Duration.ofHours(5), TaskType.DEEP_WORK), new ImportanceConstraint(5, 5));
+        Schedule after2 = new Schedule(1L, "E", "E", TIME_3, new TemporalConstraint(TIME_6, Duration.ofHours(5), TaskType.DEEP_WORK), new ImportanceConstraint(5, 5));
+        now.addDependency(before1);
+        now.addDependency(before2);
+        after1.addDependency(now);
+        after2.addDependency(now);
+        return now;
+    }
+
     public static Schedule getNotStartedSchedule() {
         return new Schedule(
                 1L,
