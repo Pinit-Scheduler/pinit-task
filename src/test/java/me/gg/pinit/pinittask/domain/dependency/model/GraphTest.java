cfeca21cdbba231c02e557b47df101ac002f70ee
GoGradually <chkun3109@gmail.com>
2025-11-10 23:49:09 +0900

    test: Graph 클래스의 의존성 관련 메서드에 대한 테스트 추가

---
+++ src/test/java/me/gg/pinit/pinittask/domain/dependency/model/GraphTest.java
@@ -0,0 +1,49 @@
+package me.gg.pinit.pinittask.domain.dependency.model;
+
+import me.gg.pinit.pinittask.domain.schedule.model.Schedule;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static me.gg.pinit.pinittask.domain.dependency.model.GraphUtils.getDependenciesFromSchedule;
+import static me.gg.pinit.pinittask.domain.dependency.model.GraphUtils.getDependenciesSample;
+import static me.gg.pinit.pinittask.domain.schedule.model.ScheduleUtils.getNotStartedSchedule;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraphTest {
+
+    @Test
+    void getNextScheduleIds() {
+        //given
+        Schedule now = getNotStartedSchedule(1L);
+        Schedule after1 = getNotStartedSchedule(2L);
+        Schedule after2 = getNotStartedSchedule(3L);
+        after1.addDependency(now);
+        after2.addDependency(now);
+
+        List<Dependency> dependencies = getDependenciesFromSchedule(after1);
+        dependencies.addAll(getDependenciesFromSchedule(after2));
+
+
+        Graph graph = Graph.of(dependencies);
+
+        //when
+        List<Long> nextScheduleIds = graph.getNextScheduleIds(now.getId());
+
+        //then
+        assertEquals(2, nextScheduleIds.size());
+        assertTrue(nextScheduleIds.contains(after1.getId()));
+        assertTrue(nextScheduleIds.contains(after2.getId()));
+    }
+
+    @Test
+    void of() {
+        //given
+        List<Dependency> dependenciesSample = getDependenciesSample();
+        //when
+        Graph graph = Graph.of(dependenciesSample);
+
+        //then
+        assertNotNull(graph);
+    }
+}
