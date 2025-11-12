4dd8cd52860c2265e35df63e2ac5532cf9595484
GoGradually <chkun3109@gmail.com>
2025-11-12 17:01:51 +0900

    feat: 일정 업데이트를 위한 낙관적 잠금 조회 메서드 추가

--- src/main/java/me/gg/pinit/pinittask/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/me/gg/pinit/pinittask/domain/schedule/repository/ScheduleRepository.java
@@ -1,7 +1,15 @@
 package me.gg.pinit.pinittask.domain.schedule.repository;
 
+import jakarta.persistence.LockModeType;
 import me.gg.pinit.pinittask.domain.schedule.model.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    @Lock(LockModeType.OPTIMISTIC)
+    @Query("SELECT s FROM Schedule s WHERE s.id = :scheduleId")
+    Optional<Schedule> findByIdForUpdate(Long scheduleId);
 }
