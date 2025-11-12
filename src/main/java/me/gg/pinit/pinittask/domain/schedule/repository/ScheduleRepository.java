1eba2388a313d55d44dd2a8250e9a4c8ae00cab2
GoGradually <chkun3109@gmail.com>
2025-11-12 22:05:41 +0900

    feat: 일정 소유자별 날짜 범위 조회 기능 추가

--- src/main/java/me/gg/pinit/pinittask/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/me/gg/pinit/pinittask/domain/schedule/repository/ScheduleRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     @Lock(LockModeType.OPTIMISTIC)
     @Query("SELECT s FROM Schedule s WHERE s.id = :scheduleId")
     Optional<Schedule> findByIdForUpdate(Long scheduleId);
+
+    List<Schedule> findAllByOwnerIdAndDateBetween(Long ownerId, ZonedDateTime start, ZonedDateTime end);
 }
