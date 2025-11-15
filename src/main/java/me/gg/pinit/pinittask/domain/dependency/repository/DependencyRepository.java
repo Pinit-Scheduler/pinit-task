5ae7672a3f3a2685a0c0ce75dc731071f6041d19
GoGradually <chkun3109@gmail.com>
2025-11-15 15:03:54 +0900

    feat: DependencyRepository에 ownerId로 모든 의존성 조회 기능 추가

--- src/main/java/me/gg/pinit/pinittask/domain/dependency/repository/DependencyRepository.java
+++ src/main/java/me/gg/pinit/pinittask/domain/dependency/repository/DependencyRepository.java
@@ -3,5 +3,8 @@ package me.gg.pinit.pinittask.domain.dependency.repository;
 import me.gg.pinit.pinittask.domain.dependency.model.Dependency;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DependencyRepository extends JpaRepository<Dependency, Long> {
+    List<Dependency> findAllByOwnerId(Long ownerId);
 }
