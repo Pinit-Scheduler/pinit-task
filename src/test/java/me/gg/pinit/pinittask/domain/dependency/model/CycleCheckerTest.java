4fc6e250d3f4b2c00e8a05d8bec009a2e7f20384
GoGradually <chkun3109@gmail.com>
2025-11-15 15:03:49 +0900

    test: CycleCheckerTest 클래스 추가 및 사이클 체크 기능 테스트 구현

---
+++ src/test/java/me/gg/pinit/pinittask/domain/dependency/model/CycleCheckerTest.java
@@ -0,0 +1,48 @@
+package me.gg.pinit.pinittask.domain.dependency.model;
+
+import me.gg.pinit.pinittask.domain.schedule.model.Schedule;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static me.gg.pinit.pinittask.domain.dependency.model.GraphUtils.getDependenciesSample;
+import static me.gg.pinit.pinittask.domain.schedule.model.ScheduleUtils.getNotStartedSchedule;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CycleCheckerTest {
+    @Test
+    void isCycleContained_NoCycle() {
+        //given
+        List<Dependency> dependenciesSample = getDependenciesSample();
+        Graph graph = Graph.of(dependenciesSample);
+
+        //when
+        boolean hasCycle = graph.hasCycle(List.of(), List.of());
+
+        //then
+        assertFalse(hasCycle);
+    }
+
+    @Test
+    void isCycleContained_Cycle() {
+        //given
+        Schedule scheduleA = getNotStartedSchedule(1L);
+        Schedule scheduleB = getNotStartedSchedule(2L);
+        Schedule scheduleC = getNotStartedSchedule(3L);
+
+        List<Dependency> dependencies = new ArrayList<>();
+        dependencies.add(new Dependency(scheduleA, scheduleB));
+        dependencies.add(new Dependency(scheduleB, scheduleC));
+
+        Graph graph = Graph.of(dependencies);
+
+        //when
+        boolean hasCycle = graph.hasCycle(List.of(), List.of(new Dependency(scheduleC, scheduleA)));
+
+        //then
+        assertTrue(hasCycle);
+    }
+
+}
