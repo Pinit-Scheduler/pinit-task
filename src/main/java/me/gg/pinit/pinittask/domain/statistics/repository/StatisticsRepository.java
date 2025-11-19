98d111c9586a588648de9fe2e9484bf3d0c34273
GoGradually <chkun3109@gmail.com>
2025-11-19 18:43:09 +0900

    feat: StatisticsRepository에 memberId와 주 시작일로 통계를 조회하는 메서드 추가

--- src/main/java/me/gg/pinit/pinittask/domain/statistics/repository/StatisticsRepository.java
+++ src/main/java/me/gg/pinit/pinittask/domain/statistics/repository/StatisticsRepository.java
@@ -3,5 +3,9 @@ package me.gg.pinit.pinittask.domain.statistics.repository;
 import me.gg.pinit.pinittask.domain.statistics.model.Statistics;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
 public interface StatisticsRepository extends JpaRepository<Statistics, Long> {
+    Optional<Statistics> findByMemberIdAndStartOfWeek(Long memberId, ZonedDateTime startOfWeek);
 }
