c22612472ea0f099f5db7974e179e435967a3537
GoGradually <chkun3109@gmail.com>
2025-11-19 18:44:04 +0900

    feat: ScheduleCanceledEventHandler 및 ScheduleCompletedEventHandler 클래스 추가

---
+++ src/main/java/me/gg/pinit/pinittask/application/statistics/event/handler/ScheduleCanceledEventHandler.java
@@ -0,0 +1,26 @@
+package me.gg.pinit.pinittask.application.statistics.event.handler;
+
+import me.gg.pinit.pinittask.application.statistics.service.StatisticsService;
+import me.gg.pinit.pinittask.domain.schedule.event.ScheduleCanceledEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+public class ScheduleCanceledEventHandler {
+    private final StatisticsService statisticsService;
+
+    public ScheduleCanceledEventHandler(StatisticsService statisticsService) {
+        this.statisticsService = statisticsService;
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void on(ScheduleCanceledEvent event) {
+        statisticsService.removeElapsedTime(
+                event.getOwnerId(),
+                event.getTaskType(),
+                event.getDuration(),
+                event.getStartTime()
+        );
+    }
+}
---
+++ src/main/java/me/gg/pinit/pinittask/application/statistics/event/handler/ScheduleCompletedEventHandler.java
@@ -0,0 +1,26 @@
+package me.gg.pinit.pinittask.application.statistics.event.handler;
+
+import me.gg.pinit.pinittask.application.statistics.service.StatisticsService;
+import me.gg.pinit.pinittask.domain.schedule.event.ScheduleCompletedEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+public class ScheduleCompletedEventHandler {
+    private final StatisticsService statisticsService;
+
+    public ScheduleCompletedEventHandler(StatisticsService statisticsService) {
+        this.statisticsService = statisticsService;
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void on(ScheduleCompletedEvent event) {
+        statisticsService.addElapsedTime(
+                event.getOwnerId(),
+                event.getTaskType(),
+                event.getDuration(),
+                event.getStartTime()
+        );
+    }
+}
