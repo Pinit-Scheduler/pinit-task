9dd728c375c7ccc5061f0a86363ea58d3c093b23
GoGradually <chkun3109@gmail.com>
2025-11-19 18:52:39 +0900

    feat: DependencyService에 스케줄 관련 의존성 삭제 메서드 수정 및 트랜잭션 추가

--- src/main/java/me/gg/pinit/pinittask/application/dependency/service/DependencyService.java
+++ src/main/java/me/gg/pinit/pinittask/application/dependency/service/DependencyService.java
@@ -4,6 +4,7 @@ import me.gg.pinit.pinittask.domain.dependency.model.Dependency;
 import me.gg.pinit.pinittask.domain.dependency.model.Graph;
 import me.gg.pinit.pinittask.domain.dependency.repository.DependencyRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,42 +16,41 @@ public class DependencyService {
         this.dependencyRepository = dependencyRepository;
     }
 
+    @Transactional(readOnly = true)
     public boolean checkCycle(Long memberId, List<Dependency> removedDependencies, List<Dependency> addedDependencies) {
         List<Dependency> allByOwnerId = dependencyRepository.findAllByOwnerId(memberId);
         Graph graph = Graph.of(allByOwnerId);
         return graph.hasCycle(removedDependencies, addedDependencies);
     }
 
+    @Transactional(readOnly = true)
     public boolean isBeforeCompleted(Long memberId, Long scheduleId) {
         List<Dependency> allByOwnerId = dependencyRepository.findAllByOwnerId(memberId);
         Graph graph = Graph.of(allByOwnerId);
         return graph.isBeforeCompleted(scheduleId);
     }
 
+    @Transactional(readOnly = true)
     public List<Long> getNextScheduleIds(Long memberId, Long scheduleId) {
         List<Dependency> allByOwnerId = dependencyRepository.findAllByOwnerId(memberId);
         Graph graph = Graph.of(allByOwnerId);
         return graph.getNextScheduleIds(scheduleId);
     }
 
+    @Transactional(readOnly = true)
     public List<Long> getPreviousScheduleIds(Long memberId, Long scheduleId) {
         List<Dependency> allByOwnerId = dependencyRepository.findAllByOwnerId(memberId);
         Graph graph = Graph.of(allByOwnerId);
         return graph.getPreviousScheduleIds(scheduleId);
     }
 
+    @Transactional
     public void saveAll(List<Dependency> dependencies) {
         dependencyRepository.saveAll(dependencies);
     }
 
-    public void deleteAll(List<Dependency> dependencies) {
-        dependencyRepository.deleteAll(dependencies);
-    }
-
-    public void deleteWithScheduleId(Long memberId, Long scheduleId) {
-        List<Dependency> dependenciesToDelete = dependencyRepository.findAllByOwnerId(memberId).stream()
-                .filter(dependency -> dependency.getFrom().getId().equals(scheduleId) || dependency.getTo().getId().equals(scheduleId))
-                .toList();
-        dependencyRepository.deleteAll(dependenciesToDelete);
+    @Transactional
+    public void deleteWithScheduleId(Long scheduleId) {
+        dependencyRepository.deleteAllRelatedToSchedule(scheduleId);
     }
 }
