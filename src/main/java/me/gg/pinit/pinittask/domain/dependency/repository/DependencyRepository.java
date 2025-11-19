80c6b46fdd9e9e7449d4e5f4edf0e61fcd7738ce
GoGradually <chkun3109@gmail.com>
2025-11-19 18:52:26 +0900

    feat: DependencyRepository에 스케줄 관련 의존성 삭제 메서드 추가

--- src/main/java/me/gg/pinit/pinittask/domain/dependency/repository/DependencyRepository.java
+++ src/main/java/me/gg/pinit/pinittask/domain/dependency/repository/DependencyRepository.java
@@ -2,9 +2,16 @@ package me.gg.pinit.pinittask.domain.dependency.repository;
 
 import me.gg.pinit.pinittask.domain.dependency.model.Dependency;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface DependencyRepository extends JpaRepository<Dependency, Long> {
     List<Dependency> findAllByOwnerId(Long ownerId);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("DELETE FROM Dependency d WHERE d.from.id = :scheduleId OR d.to.id = :scheduleId")
+    int deleteAllRelatedToSchedule(@Param("scheduleId") Long scheduleId);
 }
