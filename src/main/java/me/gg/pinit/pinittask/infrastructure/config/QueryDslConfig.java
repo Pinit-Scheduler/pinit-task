537f4c81c0bc1fea9c65d0254614767eaa5f86b8
GoGradually <chkun3109@gmail.com>
2025-11-20 21:55:12 +0900

    feat: Querydsl 설정 추가 및 의존성 추가

---
+++ src/main/java/me/gg/pinit/pinittask/infrastructure/config/QueryDslConfig.java
@@ -0,0 +1,14 @@
+package me.gg.pinit.pinittask.infrastructure.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDslConfig {
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
+}
