262f397446e4a02c7c9faf6168fd9f22a242d234
GoGradually <chkun3109@gmail.com>
2025-11-20 22:35:20 +0900

    feat: ScheduleAdjustmentService 클래스 추가 및 스케줄 조정 기능 구현

---
+++ src/main/java/me/gg/pinit/pinittask/application/scheduleadjustment/service/ScheduleAdjustmentService.java
@@ -0,0 +1,44 @@
+package me.gg.pinit.pinittask.application.scheduleadjustment.service;
+
+import me.gg.pinit.pinittask.application.dependency.service.DependencyService;
+import me.gg.pinit.pinittask.application.schedule.service.ScheduleService;
+import me.gg.pinit.pinittask.application.scheduleadjustment.dto.ScheduleDependencyAdjustCommand;
+import me.gg.pinit.pinittask.domain.dependency.model.Dependency;
+import me.gg.pinit.pinittask.domain.schedule.model.Schedule;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ScheduleAdjustmentService {
+
+    private final DependencyService dependencyService;
+    private final ScheduleService scheduleService;
+
+    public ScheduleAdjustmentService(DependencyService dependencyService, ScheduleService scheduleService) {
+        this.dependencyService = dependencyService;
+        this.scheduleService = scheduleService;
+    }
+
+    @Transactional
+    public void createSchedule(Long memberId, ScheduleDependencyAdjustCommand command) {
+        Schedule temporalSchedule = command.getTemporalSchedule();
+        List<Dependency> addedDependencies = command.getAddDependencies();
+        dependencyService.checkCycle(memberId, List.of(), addedDependencies);
+
+        scheduleService.addSchedule(temporalSchedule);
+        dependencyService.saveAll(addedDependencies);
+    }
+
+    @Transactional
+    public void adjustSchedule(Long memberId, ScheduleDependencyAdjustCommand command) {
+        List<Dependency> removedDependencies = command.getRemoveDependencies();
+        List<Dependency> addedDependencies = command.getAddDependencies();
+        dependencyService.checkCycle(memberId, removedDependencies, addedDependencies);
+
+        scheduleService.updateSchedule(memberId, command.getScheduleId(), command.getSchedulePatch());
+        dependencyService.deleteAll(removedDependencies);
+        dependencyService.saveAll(addedDependencies);
+    }
+}
