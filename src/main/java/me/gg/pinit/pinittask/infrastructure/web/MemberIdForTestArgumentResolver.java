d630e057b4b81bc610988e45764a027d1f79857a
GoGradually <chkun3109@gmail.com>
2025-11-23 18:23:15 +0900

    fix: MemberId 어노테이션 런타임 유지 및 MemberIdForTestArgumentResolver 구현

--- src/main/java/me/gg/pinit/pinittask/infrastructure/web/MemberIdForTestArgumentResolver.java
+++ src/main/java/me/gg/pinit/pinittask/infrastructure/web/MemberIdForTestArgumentResolver.java
@@ -1,5 +1,8 @@
 package me.gg.pinit.pinittask.infrastructure.web;
 
+import jakarta.annotation.PostConstruct;
+import me.gg.pinit.pinittask.domain.member.model.Member;
+import me.gg.pinit.pinittask.domain.member.repository.MemberRepository;
 import me.gg.pinit.pinittask.interfaces.utils.MemberId;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
@@ -8,15 +11,29 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import java.time.Duration;
+import java.time.ZoneId;
+
 @Component
 public class MemberIdForTestArgumentResolver implements HandlerMethodArgumentResolver {
-    @Override
-    public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.hasParameterAnnotation(MemberId.class) && parameter.getParameterType().equals(Long.class);
+    private final MemberRepository memberRepository;
+
+    public MemberIdForTestArgumentResolver(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(MemberId.class);
+    }
+
+    @Override
+    public Long resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         return 1L;
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("sample", "sample", Duration.ofHours(4), ZoneId.of("Asia/Seoul")));
     }
 }
--- src/main/java/me/gg/pinit/pinittask/interfaces/utils/MemberId.java
+++ src/main/java/me/gg/pinit/pinittask/interfaces/utils/MemberId.java
@@ -1,9 +1,17 @@
 package me.gg.pinit.pinittask.interfaces.utils;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.PARAMETER;
+
 /**
  * 멤버 ID 를 나타내는 어노테이션이다.
  * 추후 멤버 인증 과정에서 해당 멤버 ID를 추출하여 ArgumentResolver 등에서 활용할 수 있다.
  * 현재는 미구현이지만, 실제 인증 로직이 추가되면 이 어노테이션을 통해 멤버 ID를 주입받을 수 있다.
  */
+@Target({PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface MemberId {
 }
