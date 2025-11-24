81202ce72f95e3b7fa3067ba8dfaf881b9a6686f
GoGradually <chkun3109@gmail.com>
2025-11-24 14:58:19 +0900

    fix: CORS 설정에서 PATCH 메서드 추가

--- src/main/java/me/gg/pinit/pinittask/interfaces/config/WebConfig.java
+++ src/main/java/me/gg/pinit/pinittask/interfaces/config/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173") // 허용할 origin 목록
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
