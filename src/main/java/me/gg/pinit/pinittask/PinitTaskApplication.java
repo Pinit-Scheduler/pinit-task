1a41e5ddcd1273cb314501d803666e4f7b236d25
GoGradually <chkun3109@gmail.com>
2025-11-24 17:51:28 +0900

    feat: PinitTaskApplication에 비동기 처리 활성화 추가

--- src/main/java/me/gg/pinit/pinittask/PinitTaskApplication.java
+++ src/main/java/me/gg/pinit/pinittask/PinitTaskApplication.java
@@ -2,8 +2,10 @@ package me.gg.pinit.pinittask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PinitTaskApplication {
 
     public static void main(String[] args) {
