9ff1f0354b36b392ce7f035e247a0df6934483fe
GoGradually <chkun3109@gmail.com>
2025-11-24 17:18:34 +0900

    feat: ScheduleDeletedEventHandler에 비동기 처리 추가

--- src/main/java/me/gg/pinit/pinittask/application/dependency/event/ScheduleDeletedEventHandler.java
+++ src/main/java/me/gg/pinit/pinittask/application/dependency/event/ScheduleDeletedEventHandler.java
@@ -2,6 +2,7 @@ package me.gg.pinit.pinittask.application.dependency.event;
 
 import me.gg.pinit.pinittask.application.dependency.service.DependencyService;
 import me.gg.pinit.pinittask.domain.schedule.event.ScheduleDeletedEvent;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -14,6 +15,7 @@ public class ScheduleDeletedEventHandler {
         this.dependencyService = dependencyService;
     }
 
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void on(ScheduleDeletedEvent event) {
         dependencyService.deleteWithScheduleId(event.getScheduleId());
