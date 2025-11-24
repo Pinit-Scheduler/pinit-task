d8cf7bd497ad913a8d64fa56c18f625844e8cd31
GoGradually <chkun3109@gmail.com>
2025-11-24 17:03:47 +0900

    feat: ScheduleCanceledEventHandler와 ScheduleCompletedEventHandler에 비동기 처리 추가

--- src/main/java/me/gg/pinit/pinittask/application/statistics/event/handler/ScheduleCanceledEventHandler.java
+++ src/main/java/me/gg/pinit/pinittask/application/statistics/event/handler/ScheduleCanceledEventHandler.java
@@ -2,6 +2,7 @@ package me.gg.pinit.pinittask.application.statistics.event.handler;
 
 import me.gg.pinit.pinittask.application.statistics.service.StatisticsService;
 import me.gg.pinit.pinittask.domain.schedule.event.ScheduleCanceledEvent;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -13,7 +14,7 @@ public class ScheduleCanceledEventHandler {
     public ScheduleCanceledEventHandler(StatisticsService statisticsService) {
         this.statisticsService = statisticsService;
     }
-
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void on(ScheduleCanceledEvent event) {
         statisticsService.removeElapsedTime(
--- src/main/java/me/gg/pinit/pinittask/application/statistics/event/handler/ScheduleCompletedEventHandler.java
+++ src/main/java/me/gg/pinit/pinittask/application/statistics/event/handler/ScheduleCompletedEventHandler.java
@@ -2,6 +2,7 @@ package me.gg.pinit.pinittask.application.statistics.event.handler;
 
 import me.gg.pinit.pinittask.application.statistics.service.StatisticsService;
 import me.gg.pinit.pinittask.domain.schedule.event.ScheduleCompletedEvent;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -14,6 +15,7 @@ public class ScheduleCompletedEventHandler {
         this.statisticsService = statisticsService;
     }
 
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void on(ScheduleCompletedEvent event) {
         statisticsService.addElapsedTime(
