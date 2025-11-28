e57ac99fa78dfe5843631c35a3df2f1f5186ea2a
GoGradually <chkun3109@gmail.com>
2025-11-28 22:28:51 +0900

    feat: 전역 예외 처리를 위한 GlobalControllerAdvice 클래스 추가

---
+++ src/main/java/me/gg/pinit/pinittask/interfaces/exception/GlobalControllerAdvice.java
@@ -0,0 +1,24 @@
+package me.gg.pinit.pinittask.interfaces.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalControllerAdvice {
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoResourceFound(NoResourceFoundException ex, WebRequest request) {
+        log.warn("No resource found: {}", ex.getMessage());
+        return buildResponse(HttpStatus.NOT_FOUND, ex.getMessage(), request);
+    }
+
+    private ResponseEntity<ErrorResponse> buildResponse(HttpStatus status, String message, WebRequest request) {
+        ErrorResponse body = ErrorResponse.of(status.value(), status.getReasonPhrase(), message, request.getDescription(false));
+        return ResponseEntity.status(status).body(body);
+    }
+}
