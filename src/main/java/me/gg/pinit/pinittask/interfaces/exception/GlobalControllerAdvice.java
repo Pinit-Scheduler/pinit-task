5e25ece161286b868dd028839a8dd0b85f30c17f
GoGradually <chkun3109@gmail.com>
2025-12-01 11:51:29 +0900

    feat: 누락된 요청 파라미터 처리 예외 핸들러 추가

--- src/main/java/me/gg/pinit/pinittask/interfaces/exception/GlobalControllerAdvice.java
+++ src/main/java/me/gg/pinit/pinittask/interfaces/exception/GlobalControllerAdvice.java
@@ -3,6 +3,7 @@ package me.gg.pinit.pinittask.interfaces.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -15,6 +16,12 @@ public class GlobalControllerAdvice {
     public ResponseEntity<ErrorResponse> handleNoResourceFound(NoResourceFoundException ex, WebRequest request) {
         log.warn("No resource found: {}", ex.getMessage());
         return buildResponse(HttpStatus.NOT_FOUND, ex.getMessage(), request);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ErrorResponse> handleMissingServletRequestParameter(MissingServletRequestParameterException ex, WebRequest request) {
+        log.warn("Missing request parameter: {}", ex.getMessage());
+        return buildResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), request);
     }
 
     private ResponseEntity<ErrorResponse> buildResponse(HttpStatus status, String message, WebRequest request) {
