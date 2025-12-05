3941b7a1ef9f8656d33c1958f6aec036bbdf5f72
GoGradually <chkun3109@gmail.com>
2025-12-06 02:24:36 +0900

    feat: JWT 토큰을 이용한 사용자 ID 해석 기능 추가

--- src/main/java/me/gg/pinit/pinittask/infrastructure/web/MemberIdForTestArgumentResolver.java
+++ src/main/java/me/gg/pinit/pinittask/infrastructure/web/MemberIdForTestArgumentResolver.java
@@ -1,8 +1,12 @@
 package me.gg.pinit.pinittask.infrastructure.web;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import me.gg.pinit.pinittask.domain.member.exception.MemberNotFoundException;
 import me.gg.pinit.pinittask.domain.member.model.Member;
 import me.gg.pinit.pinittask.domain.member.repository.MemberRepository;
+import me.gg.pinit.pinittask.infrastructure.authenticate.JwtTokenProvider;
 import me.gg.pinit.pinittask.interfaces.utils.MemberId;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
@@ -13,22 +17,48 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 import java.time.Duration;
 import java.time.ZoneId;
-
+import java.util.Objects;
+@Slf4j
 @Component
 public class MemberIdForTestArgumentResolver implements HandlerMethodArgumentResolver {
-    private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public MemberIdForTestArgumentResolver(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    public MemberIdForTestArgumentResolver(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.hasParameterAnnotation(MemberId.class);
+        return parameter.hasParameterAnnotation(MemberId.class)
+                && (Long.class.isAssignableFrom(parameter.getParameterType())
+                || long.class.isAssignableFrom(parameter.getParameterType()));
     }
 
     @Override
-    public Long resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        return 1L;
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        if (request == null) {
+            log.info("HttpServletRequest is null");
+            throw new MemberNotFoundException("사용자 정보를 찾을 수 없습니다.");
+        }
+
+        String token = resolveToken(request);
+        if (token == null || !jwtTokenProvider.validateToken(token)) {
+            throw new MemberNotFoundException("사용자 정보를 찾을 수 없습니다.");
+        }
+
+        return jwtTokenProvider.getMemberId(token);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
     }
 }
