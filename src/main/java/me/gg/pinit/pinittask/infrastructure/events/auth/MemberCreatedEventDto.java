55ba4aec3666fa8c4c05c675d3293faea598c8c7
GoGradually <chkun3109@gmail.com>
2025-12-09 00:33:45 +0900

    feat: MemberCreatedEventDto 및 MemberCreatedEventListener 클래스 추가

---
+++ src/main/java/me/gg/pinit/pinittask/infrastructure/events/auth/MemberCreatedEventDto.java
@@ -0,0 +1,4 @@
+package me.gg.pinit.pinittask.infrastructure.events.auth;
+
+public record MemberCreatedEventDto(Long memberId, String nickname) {
+}
---
+++ src/main/java/me/gg/pinit/pinittask/infrastructure/events/auth/MemberCreatedEventListener.java
@@ -0,0 +1,21 @@
+package me.gg.pinit.pinittask.infrastructure.events.auth;
+
+
+import me.gg.pinit.pinittask.application.member.service.MemberService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberCreatedEventListener {
+    private final MemberService memberService;
+
+    public MemberCreatedEventListener(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @RabbitListener(queues = AuthMemberMessaging.MEMBER_CREATED_QUEUE)
+    public void on(MemberCreatedEventDto event) {
+        memberService.enrollMember(event.memberId(), event.nickname());
+    }
+
+}
