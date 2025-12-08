a15e0a87b7442966c4487836b98b95b0e20fede1
GoGradually <chkun3109@gmail.com>
2025-12-08 23:53:39 +0900

    feat: AuthMemberMessaging 클래스 추가 및 메시징 상수 정의

---
+++ src/main/java/me/gg/pinit/pinittask/infrastructure/events/messaging/AuthMemberMessaging.java
@@ -0,0 +1,7 @@
+package me.gg.pinit.pinittask.infrastructure.events.messaging;
+
+public class AuthMemberMessaging {
+    public static final String DIRECT_EXCHANGE = "auth.member.direct";
+    public static final String BK_MEMBER_CREATED = "member.created";
+}
+
