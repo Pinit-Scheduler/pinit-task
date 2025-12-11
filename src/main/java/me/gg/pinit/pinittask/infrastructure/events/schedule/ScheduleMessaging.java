9cb7d8f50d0a5ba0a0fcf36ef06c587be8ad80f5
GoGradually <chkun3109@gmail.com>
2025-12-12 05:04:42 +0900

    feat: 일정 시간 업데이트 이벤트 키 변경

--- src/main/java/me/gg/pinit/pinittask/infrastructure/events/schedule/ScheduleMessaging.java
+++ src/main/java/me/gg/pinit/pinittask/infrastructure/events/schedule/ScheduleMessaging.java
@@ -2,8 +2,7 @@ package me.gg.pinit.pinittask.infrastructure.events.schedule;
 
 public class ScheduleMessaging {
     public static final String DIRECT_EXCHANGE = "task.schedule.direct";
-    public static final String RK_SCHEDULE_TIME_CHANGED = "schedule.time.upcoming.changed";
-    public static final String RK_SCHEDULE_TIME_ENROLLED = "schedule.time.upcoming.enrolled";
+    public static final String RK_SCHEDULE_TIME_UPDATED = "schedule.time.upcoming.updated";
     public static final String RK_SCHEDULE_DELETED = "schedule.deleted";
     public static final String RK_SCHEDULE_CANCELED = "schedule.state.canceled";
     public static final String RK_SCHEDULE_STARTED = "schedule.state.started";
