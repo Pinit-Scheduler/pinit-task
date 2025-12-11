3684e5605306a9bfd74b8010b050881523464a9b
GoGradually <chkun3109@gmail.com>
2025-12-12 02:44:25 +0900

    feat: MemberCreatedEventDto에 occurredAt 필드 추가

--- src/main/java/me/gg/pinit/pinittask/infrastructure/events/auth/MemberCreatedEventDto.java
+++ src/main/java/me/gg/pinit/pinittask/infrastructure/events/auth/MemberCreatedEventDto.java
@@ -1,4 +1,6 @@
 package me.gg.pinit.pinittask.infrastructure.events.auth;
 
-public record MemberCreatedEventDto(Long memberId, String nickname) {
+import java.time.LocalDateTime;
+
+public record MemberCreatedEventDto(Long memberId, String nickname, LocalDateTime occurredAt) {
 }
