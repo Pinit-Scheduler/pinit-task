0757033f2053ca78daf8f5fbf077366f66a47658
GoGradually <chkun3109@gmail.com>
2025-12-12 00:13:28 +0900

    feat: ScheduleStartedEvent 클래스 추가

---
+++ src/main/java/me/gg/pinit/pinittask/domain/schedule/event/ScheduleStartedEvent.java
@@ -0,0 +1,17 @@
+package me.gg.pinit.pinittask.domain.schedule.event;
+
+import lombok.Getter;
+import me.gg.pinit.pinittask.domain.events.DomainEvent;
+
+@Getter
+public class ScheduleStartedEvent implements DomainEvent {
+    private final Long scheduleId;
+    private final Long ownerId;
+    private final String beforeState;
+
+    public ScheduleStartedEvent(Long scheduleId, Long ownerId, String beforeState) {
+        this.scheduleId = scheduleId;
+        this.ownerId = ownerId;
+        this.beforeState = beforeState;
+    }
+}
