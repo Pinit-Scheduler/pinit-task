02c7a175fa20332d5ed13c405b1d8e15d3408464
GoGradually <chkun3109@gmail.com>
2025-12-12 00:15:52 +0900

    feat: ScheduleCompletedEventHandler에서 ScheduleService 추가 및 통계 계산 로직 수정

--- src/main/java/me/gg/pinit/pinittask/application/statistics/event/handler/ScheduleCompletedEventHandler.java
+++ src/main/java/me/gg/pinit/pinittask/application/statistics/event/handler/ScheduleCompletedEventHandler.java
@@ -1,7 +1,9 @@
 package me.gg.pinit.pinittask.application.statistics.event.handler;
 
+import me.gg.pinit.pinittask.application.schedule.service.ScheduleService;
 import me.gg.pinit.pinittask.application.statistics.service.StatisticsService;
 import me.gg.pinit.pinittask.domain.schedule.event.ScheduleCompletedEvent;
+import me.gg.pinit.pinittask.domain.schedule.model.Schedule;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -10,19 +12,22 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @Component
 public class ScheduleCompletedEventHandler {
     private final StatisticsService statisticsService;
+    private final ScheduleService scheduleService;
 
-    public ScheduleCompletedEventHandler(StatisticsService statisticsService) {
+    public ScheduleCompletedEventHandler(StatisticsService statisticsService, ScheduleService scheduleService) {
         this.statisticsService = statisticsService;
+        this.scheduleService = scheduleService;
     }
 
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void on(ScheduleCompletedEvent event) {
+        Schedule schedule = scheduleService.getSchedule(event.getOwnerId(), event.getScheduleId());
         statisticsService.addElapsedTime(
                 event.getOwnerId(),
-                event.getTaskType(),
-                event.getDuration(),
-                event.getStartTime()
+                schedule.getTemporalConstraint().getTaskType(),
+                schedule.getHistory().getElapsedTime(),
+                schedule.getHistory().getStartTime()
         );
     }
 }
