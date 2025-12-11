1761f56beaa0904b42d2df1e901f6a6358f6e7da
GoGradually <chkun3109@gmail.com>
2025-12-12 06:00:32 +0900

    docs: 기록 메서드의 의도 기

--- src/main/java/me/gg/pinit/pinittask/domain/schedule/model/Recorder.java
+++ src/main/java/me/gg/pinit/pinittask/domain/schedule/model/Recorder.java
@@ -5,6 +5,15 @@ import me.gg.pinit.pinittask.domain.statistics.model.Statistics;
 import java.time.Duration;
 
 public interface Recorder {
+
+    /**
+     * 특정 사용자의 특정 주차 통계에 경과 시간 추가
+     * 통계는 반드시 완료된 일정에 대해서만 기록되어야 함
+     * 해당 일정이 완료되기 전까지는 해당 일정이 집중 작업인지, 행정 작업인지, 빠른 작업인지 알 수 없기 때문
+     *
+     * @param statistics 해당 주차 통계
+     * @param duration   추가할 경과 시간
+     */
     void record(Statistics statistics, Duration duration);
 
     void rollback(Statistics statistics, Duration duration);
