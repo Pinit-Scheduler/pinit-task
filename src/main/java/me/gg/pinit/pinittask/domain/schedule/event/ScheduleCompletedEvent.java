8fdc4ee6dc9dc26bdd201478263b74de9fbb1391
GoGradually <chkun3109@gmail.com>
2025-12-12 00:08:15 +0900

    feat: ScheduleCompletedEvent에 scheduleId 필드 추가 및 생성자 수정

--- src/main/java/me/gg/pinit/pinittask/domain/schedule/event/ScheduleCompletedEvent.java
+++ src/main/java/me/gg/pinit/pinittask/domain/schedule/event/ScheduleCompletedEvent.java
@@ -2,22 +2,16 @@ package me.gg.pinit.pinittask.domain.schedule.event;
 
 import lombok.Getter;
 import me.gg.pinit.pinittask.domain.events.DomainEvent;
-import me.gg.pinit.pinittask.domain.schedule.model.TaskType;
-
-import java.time.Duration;
-import java.time.ZonedDateTime;
 
 @Getter
 public class ScheduleCompletedEvent implements DomainEvent {
+    private final Long scheduleId;
     private final Long ownerId;
-    private final TaskType taskType;
-    private final Duration duration;
-    private final ZonedDateTime startTime;
+    private final String beforeState;
 
-    public ScheduleCompletedEvent(Long ownerId, TaskType taskType, Duration duration, ZonedDateTime startTime) {
+    public ScheduleCompletedEvent(Long scheduleId, Long ownerId, String beforeState) {
+        this.scheduleId = scheduleId;
         this.ownerId = ownerId;
-        this.taskType = taskType;
-        this.duration = duration;
-        this.startTime = startTime;
+        this.beforeState = beforeState;
     }
 }
