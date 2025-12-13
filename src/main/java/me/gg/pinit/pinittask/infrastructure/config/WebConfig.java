17876d098b5b1b231091cbd8880e24a87be405d7
GoGradually <chkun3109@gmail.com>
2025-12-13 19:33:30 +0900

    feat: 이벤트 큐 클리어 인터셉터를 WebConfig에 추가

--- src/main/java/me/gg/pinit/pinittask/infrastructure/config/WebConfig.java
+++ src/main/java/me/gg/pinit/pinittask/infrastructure/config/WebConfig.java
@@ -1,8 +1,10 @@
 package me.gg.pinit.pinittask.infrastructure.config;
 
+import me.gg.pinit.pinittask.infrastructure.web.EventQueueClearInterceptor;
 import me.gg.pinit.pinittask.infrastructure.web.MemberIdArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -11,9 +13,11 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private final MemberIdArgumentResolver memberIdArgumentResolver;
+    private final EventQueueClearInterceptor eventQueueClearInterceptor;
 
-    public WebConfig(MemberIdArgumentResolver memberIdArgumentResolver) {
+    public WebConfig(MemberIdArgumentResolver memberIdArgumentResolver, EventQueueClearInterceptor eventQueueClearInterceptor) {
         this.memberIdArgumentResolver = memberIdArgumentResolver;
+        this.eventQueueClearInterceptor = eventQueueClearInterceptor;
     }
 
     @Override
@@ -21,4 +25,8 @@ public class WebConfig implements WebMvcConfigurer {
         resolvers.add(memberIdArgumentResolver);
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(eventQueueClearInterceptor);
+    }
 }
