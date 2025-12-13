b6101ceaaf3a95be62af61f6f0bcb6e8cea4f48d
GoGradually <chkun3109@gmail.com>
2025-12-13 19:32:17 +0900

    feat: 이벤트 큐를 클리어하는 인터셉터 추가

---
+++ src/main/java/me/gg/pinit/pinittask/infrastructure/web/EventQueueClearInterceptor.java
@@ -0,0 +1,17 @@
+package me.gg.pinit.pinittask.infrastructure.web;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import me.gg.pinit.pinittask.domain.events.DomainEvents;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class EventQueueClearInterceptor implements HandlerInterceptor {
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Object handler, Exception ex) throws Exception {
+        DomainEvents.getEventsAndClear();
+    }
+}
