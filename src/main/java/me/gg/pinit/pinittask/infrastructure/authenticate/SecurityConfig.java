1f421df447a4b112a2b29adf8fc9c379d36a97e4
GoGradually <chkun3109@gmail.com>
2025-12-20 01:36:11 +0900

    feat: Actuator 헬스 체크 경로에 대한 접근 허용 추가

--- src/main/java/me/gg/pinit/pinittask/infrastructure/authenticate/SecurityConfig.java
+++ src/main/java/me/gg/pinit/pinittask/infrastructure/authenticate/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                         (request, response, ex) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid or missing token")
                 ))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/signup", "/refresh", "/login/**", "/v3/**", "/swagger-ui/**", "/async-api/**").permitAll()
+                        .requestMatchers("/actuator/health/**", "/v3/**", "/swagger-ui/**", "/async-api/**").permitAll()
                         .anyRequest().authenticated()
                 );
 
