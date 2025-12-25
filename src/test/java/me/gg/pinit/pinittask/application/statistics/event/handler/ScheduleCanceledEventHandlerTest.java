104b2a644e62dafee3821f1a7990055037e7e7cc
GoGradually <chkun3109@gmail.com>
2025-12-25 18:24:45 +0900

    test: ScheduleCanceledEventHandler 클래스에 대한 단위 테스트 추가

---
+++ src/test/java/me/gg/pinit/pinittask/application/statistics/event/handler/ScheduleCanceledEventHandlerTest.java
@@ -0,0 +1,67 @@
+package me.gg.pinit.pinittask.application.statistics.event.handler;
+
+import me.gg.pinit.pinittask.application.schedule.service.ScheduleService;
+import me.gg.pinit.pinittask.application.statistics.service.StatisticsService;
+import me.gg.pinit.pinittask.domain.schedule.event.ScheduleCanceledEvent;
+import me.gg.pinit.pinittask.domain.schedule.model.CompletedState;
+import me.gg.pinit.pinittask.domain.schedule.model.Schedule;
+import me.gg.pinit.pinittask.domain.schedule.model.TaskType;
+import me.gg.pinit.pinittask.domain.schedule.vo.ScheduleHistory;
+import me.gg.pinit.pinittask.domain.schedule.vo.TemporalConstraint;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Duration;
+import java.time.ZonedDateTime;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ScheduleCanceledEventHandlerTest {
+    private final Long scheduleId = 11L;
+    private final Long ownerId = 22L;
+    @Mock
+    StatisticsService statisticsService;
+    @Mock
+    ScheduleService scheduleService;
+    @Mock
+    Schedule schedule;
+    @InjectMocks
+    ScheduleCanceledEventHandler handler;
+
+    @Test
+    void ignoresCancelWhenScheduleWasNotCompleted() {
+        //given
+        ScheduleCanceledEvent event = new ScheduleCanceledEvent(scheduleId, ownerId, "IN_PROGRESS");
+
+        //when
+        handler.on(event);
+
+        //then
+        verifyNoInteractions(scheduleService, statisticsService);
+    }
+
+    @Test
+    void removesElapsedTimeWhenCompletedScheduleIsCanceled() {
+        //given
+        ZonedDateTime designatedStart = ZonedDateTime.now();
+        TemporalConstraint temporalConstraint = new TemporalConstraint(designatedStart.plusDays(1), Duration.ofHours(1), TaskType.ADMIN_TASK);
+        ScheduleHistory history = new ScheduleHistory(null, Duration.ofMinutes(30));
+        ScheduleCanceledEvent event = new ScheduleCanceledEvent(scheduleId, ownerId, CompletedState.COMPLETED);
+
+        when(scheduleService.getSchedule(ownerId, scheduleId)).thenReturn(schedule);
+        when(schedule.getTemporalConstraint()).thenReturn(temporalConstraint);
+        when(schedule.getHistory()).thenReturn(history);
+        when(schedule.getDesignatedStartTime()).thenReturn(designatedStart);
+
+        //when
+        handler.on(event);
+
+        //then
+        verify(scheduleService).getSchedule(ownerId, scheduleId);
+        verify(statisticsService).removeElapsedTime(ownerId, TaskType.ADMIN_TASK, history.getElapsedTime(), designatedStart);
+    }
+}
