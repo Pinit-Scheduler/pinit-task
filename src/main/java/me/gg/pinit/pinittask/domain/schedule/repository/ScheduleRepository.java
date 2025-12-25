3e331293dcf9e0354b3779c2937c041e2b690a78
GoGradually <chkun3109@gmail.com>
2025-12-25 10:53:48 +0900

    feat: ScheduleRepository에 사용자 정의 인터페이스 추가 및 사용하지 않는 메소드 제거

--- src/main/java/me/gg/pinit/pinittask/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/me/gg/pinit/pinittask/domain/schedule/repository/ScheduleRepository.java
@@ -6,15 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
-public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+public interface ScheduleRepository extends JpaRepository<Schedule, Long>, ScheduleRepositoryCustom {
     @Lock(LockModeType.OPTIMISTIC)
     @Query("SELECT s FROM Schedule s WHERE s.id = :scheduleId")
     Optional<Schedule> findByIdForUpdate(Long scheduleId);
-
-    @Query("SELECT s FROM Schedule s WHERE s.ownerId = :ownerId AND s.designatedStartTime.dateTime >= :start AND s.designatedStartTime.dateTime < :end AND s.designatedStartTime.zoneId = :zoneId")
-    List<Schedule> findAllByOwnerIdAndDesignatedStartTimeBetween(Long ownerId, LocalDateTime start, LocalDateTime end, String zoneId);
 }
