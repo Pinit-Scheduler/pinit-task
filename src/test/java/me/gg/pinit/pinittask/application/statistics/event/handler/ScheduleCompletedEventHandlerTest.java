ee4aa041060ae9889ecb897679e67caf1294a179
GoGradually <chkun3109@gmail.com>
2026-01-21 00:54:53 +0900

    feat: 일정 완료 이벤트 핸들러에 작업 서비스 추가

--- src/test/java/me/gg/pinit/pinittask/application/statistics/event/handler/ScheduleCompletedEventHandlerTest.java
+++ src/test/java/me/gg/pinit/pinittask/application/statistics/event/handler/ScheduleCompletedEventHandlerTest.java
@@ -2,11 +2,13 @@ package me.gg.pinit.pinittask.application.statistics.event.handler;
 
 import me.gg.pinit.pinittask.application.schedule.service.ScheduleService;
 import me.gg.pinit.pinittask.application.statistics.service.StatisticsService;
+import me.gg.pinit.pinittask.application.task.service.TaskService;
 import me.gg.pinit.pinittask.domain.schedule.event.ScheduleCompletedEvent;
 import me.gg.pinit.pinittask.domain.schedule.model.Schedule;
-import me.gg.pinit.pinittask.domain.schedule.model.TaskType;
 import me.gg.pinit.pinittask.domain.schedule.vo.ScheduleHistory;
-import me.gg.pinit.pinittask.domain.schedule.vo.TemporalConstraint;
+import me.gg.pinit.pinittask.domain.task.model.Task;
+import me.gg.pinit.pinittask.domain.task.model.TaskType;
+import me.gg.pinit.pinittask.domain.task.model.TaskUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -28,6 +30,8 @@ class ScheduleCompletedEventHandlerTest {
     @Mock
     ScheduleService scheduleService;
     @Mock
+    TaskService taskService;
+    @Mock
     Schedule schedule;
     @InjectMocks
     ScheduleCompletedEventHandler handler;
@@ -36,14 +40,15 @@ class ScheduleCompletedEventHandlerTest {
     void addsElapsedTimeForCompletedSchedule() {
         //given
         ZonedDateTime designatedStart = ZonedDateTime.now();
-        TemporalConstraint temporalConstraint = new TemporalConstraint(designatedStart.plusDays(1), Duration.ofHours(1), TaskType.DEEP_WORK);
         ScheduleHistory history = new ScheduleHistory(null, Duration.ofMinutes(45));
         ScheduleCompletedEvent event = new ScheduleCompletedEvent(scheduleId, ownerId, "IN_PROGRESS");
+        Task task = TaskUtils.newTask(ownerId, 33L);
 
         when(scheduleService.getSchedule(ownerId, scheduleId)).thenReturn(schedule);
-        when(schedule.getTemporalConstraint()).thenReturn(temporalConstraint);
+        when(schedule.getTaskId()).thenReturn(task.getId());
         when(schedule.getHistory()).thenReturn(history);
         when(schedule.getDesignatedStartTime()).thenReturn(designatedStart);
+        when(taskService.getTask(ownerId, task.getId())).thenReturn(task);
 
         //when
         handler.on(event);
