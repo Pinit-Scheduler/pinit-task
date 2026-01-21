c15b832a01b39d160080ccbfb4c5940405660664
GoGradually <chkun3109@gmail.com>
2026-01-21 15:41:48 +0900

    feat: 일정 패치 클래스에서 불필요한 필드 및 메서드 제거

--- src/main/java/me/gg/pinit/pinittask/domain/schedule/patch/SchedulePatch.java
+++ src/main/java/me/gg/pinit/pinittask/domain/schedule/patch/SchedulePatch.java
@@ -1,18 +1,12 @@
 package me.gg.pinit.pinittask.domain.schedule.patch;
 
-import me.gg.pinit.pinittask.domain.schedule.model.TaskType;
-
 import java.time.ZonedDateTime;
 import java.util.Optional;
 
 public final class SchedulePatch {
     private String title;
     private String description;
-    private ZonedDateTime deadline;
-    private Integer importance;
-    private Integer difficulty;
-    private TaskType taskType;
-    private ZonedDateTime date;
+    private ZonedDateTime designatedStartTime;
 
     public SchedulePatch setTitle(String v) {
         this.title = v;
@@ -24,28 +18,8 @@ public final class SchedulePatch {
         return this;
     }
 
-    public SchedulePatch setDeadline(ZonedDateTime v) {
-        this.deadline = v;
-        return this;
-    }
-
-    public SchedulePatch setImportance(Integer v) {
-        this.importance = v;
-        return this;
-    }
-
-    public SchedulePatch setDifficulty(Integer v) {
-        this.difficulty = v;
-        return this;
-    }
-
-    public SchedulePatch setTaskType(TaskType v) {
-        this.taskType = v;
-        return this;
-    }
-
-    public SchedulePatch setDate(ZonedDateTime v) {
-        this.date = v;
+    public SchedulePatch setDesignatedStartTime(ZonedDateTime v) {
+        this.designatedStartTime = v;
         return this;
     }
 
@@ -57,30 +31,12 @@ public final class SchedulePatch {
         return Optional.ofNullable(description);
     }
 
-    public Optional<ZonedDateTime> deadline() {
-        return Optional.ofNullable(deadline);
-    }
-
-    public Optional<Integer> importance() {
-        return Optional.ofNullable(importance);
-    }
-
-    public Optional<Integer> difficulty() {
-        return Optional.ofNullable(difficulty);
-    }
-
-    public Optional<TaskType> taskType() {
-        return Optional.ofNullable(taskType);
-    }
-
-    public Optional<ZonedDateTime> date() {
-        return Optional.ofNullable(date);
+    public Optional<ZonedDateTime> designatedStartTime() {
+        return Optional.ofNullable(designatedStartTime);
     }
 
     public boolean isEmpty() {
-        return title == null && description == null && deadline == null
-                && importance == null && difficulty == null && taskType == null && date == null;
+        return title == null && description == null && designatedStartTime == null;
     }
 }
-
 
