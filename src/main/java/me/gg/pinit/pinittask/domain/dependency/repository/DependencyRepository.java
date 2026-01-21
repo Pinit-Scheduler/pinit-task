0cc042afb3871c87975f48b0ee019ef82c0d2dbb
GoGradually <chkun3109@gmail.com>
2026-01-21 21:51:16 +0900

    feat: DependencyRepository에 fromId로 의존성 조회 메서드 추가

--- src/main/java/me/gg/pinit/pinittask/domain/dependency/repository/DependencyRepository.java
+++ src/main/java/me/gg/pinit/pinittask/domain/dependency/repository/DependencyRepository.java
@@ -11,6 +11,8 @@ import java.util.List;
 public interface DependencyRepository extends JpaRepository<Dependency, Long>, DependencyRepositoryCustom {
     List<Dependency> findAllByOwnerId(Long ownerId);
 
+    List<Dependency> findAllByFromId(Long fromId);
+
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("DELETE FROM Dependency d WHERE d.fromId = :taskId OR d.toId = :taskId")
     int deleteAllRelatedToTask(@Param("taskId") Long taskId);
