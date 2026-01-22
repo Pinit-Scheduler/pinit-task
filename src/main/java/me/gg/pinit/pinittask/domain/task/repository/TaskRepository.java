a8c0c853c368bb2e9067562760b69e0b50224ed5
GoGradually <chkun3109@gmail.com>
2026-01-22 16:40:07 +0900

    feat: TaskRepository에 ownerId로 조회하는 메서드 및 낙관적 락 조회 메서드 추가

--- src/main/java/me/gg/pinit/pinittask/domain/task/repository/TaskRepository.java
+++ src/main/java/me/gg/pinit/pinittask/domain/task/repository/TaskRepository.java
@@ -1,10 +1,27 @@
 package me.gg.pinit.pinittask.domain.task.repository;
 
+import jakarta.persistence.LockModeType;
 import me.gg.pinit.pinittask.domain.task.model.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findAllByOwnerId(Long ownerId);
+
+    Page<Task> findAllByOwnerId(Long ownerId, Pageable pageable);
+
+    Page<Task> findAllByOwnerIdAndInboundDependencyCount(Long ownerId, int inboundDependencyCount, Pageable pageable);
+
+    Page<Task> findAllByOwnerIdAndInboundDependencyCountAndCompletedFalse(Long ownerId, int inboundDependencyCount, Pageable pageable);
+
+    @Lock(LockModeType.OPTIMISTIC)
+    @Query("select t from Task t where t.id in :ids")
+    List<Task> findAllByIdInWithOptimisticLock(@Param("ids") Collection<Long> ids);
 }
