fe34a33092908929dc69f1f17af661eb4019665d
GoGradually <chkun3109@gmail.com>
2026-01-22 20:34:53 +0900

    refactor: ScheduleCompletedEventHandler에서 TaskType 제거 및 ScheduleType으로 변경

--- src/main/java/me/gg/pinit/pinittask/application/statistics/event/handler/ScheduleCompletedEventHandler.java
+++ src/main/java/me/gg/pinit/pinittask/application/statistics/event/handler/ScheduleCompletedEventHandler.java
@@ -2,10 +2,8 @@ package me.gg.pinit.pinittask.application.statistics.event.handler;
 
 import me.gg.pinit.pinittask.application.schedule.service.ScheduleService;
 import me.gg.pinit.pinittask.application.statistics.service.StatisticsService;
-import me.gg.pinit.pinittask.application.task.service.TaskService;
 import me.gg.pinit.pinittask.domain.schedule.event.ScheduleCompletedEvent;
 import me.gg.pinit.pinittask.domain.schedule.model.Schedule;
-import me.gg.pinit.pinittask.domain.task.model.Task;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -15,25 +13,19 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class ScheduleCompletedEventHandler {
     private final StatisticsService statisticsService;
     private final ScheduleService scheduleService;
-    private final TaskService taskService;
 
-    public ScheduleCompletedEventHandler(StatisticsService statisticsService, ScheduleService scheduleService, TaskService taskService) {
+    public ScheduleCompletedEventHandler(StatisticsService statisticsService, ScheduleService scheduleService) {
         this.statisticsService = statisticsService;
         this.scheduleService = scheduleService;
-        this.taskService = taskService;
     }
 
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void on(ScheduleCompletedEvent event) {
         Schedule schedule = scheduleService.getSchedule(event.getOwnerId(), event.getScheduleId());
-        if (schedule.getTaskId() == null) {
-            return;
-        }
-        Task task = taskService.getTask(event.getOwnerId(), schedule.getTaskId());
         statisticsService.addElapsedTime(
                 event.getOwnerId(),
-                task.getTemporalConstraint().getTaskType(),
+                schedule.getScheduleType(),
                 schedule.getHistory().getElapsedTime(),
                 schedule.getDesignatedStartTime()
         );
