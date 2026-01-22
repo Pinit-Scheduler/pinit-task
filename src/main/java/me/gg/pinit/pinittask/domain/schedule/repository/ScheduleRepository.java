d9e3f15e076a7dc0eb82633cb42fd20ffd73f3cd
GoGradually <chkun3109@gmail.com>
2026-01-22 16:36:15 +0900

    feat: ScheduleRepository에 taskId로 조회하는 메서드 추가

--- src/main/java/me/gg/pinit/pinittask/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/me/gg/pinit/pinittask/domain/schedule/repository/ScheduleRepository.java
@@ -15,4 +15,8 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long>, Sched
     Optional<Schedule> findByIdForUpdate(Long scheduleId);
 
     List<Schedule> findAllByTaskId(Long taskId);
+
+    Optional<Schedule> findByTaskId(Long taskId);
+
+    boolean existsByTaskId(Long taskId);
 }
