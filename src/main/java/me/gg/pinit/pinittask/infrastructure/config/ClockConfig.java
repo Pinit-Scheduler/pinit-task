5ee4315c682c288185652353de987c5a790d3d0e
GoGradually <chkun3109@gmail.com>
2026-01-27 16:30:58 +0900

    feat: 시스템 시계를 위한 ClockConfig 클래스 추가

---
+++ src/main/java/me/gg/pinit/pinittask/infrastructure/config/ClockConfig.java
@@ -0,0 +1,15 @@
+package me.gg.pinit.pinittask.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+
+@Configuration
+public class ClockConfig {
+
+    @Bean
+    public Clock systemClock() {
+        return Clock.systemUTC();
+    }
+}
