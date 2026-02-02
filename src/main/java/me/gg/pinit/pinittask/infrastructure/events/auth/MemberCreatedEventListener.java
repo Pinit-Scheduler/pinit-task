bc4c35fc4396e176356f65ba22d4148a23d2c834
GoGradually <chkun3109@gmail.com>
2026-02-02 23:08:37 +0900

    feat: MemberCreatedEventListener에서 시간대 정보를 Asia/Seoul로 설정

--- src/main/java/me/gg/pinit/pinittask/infrastructure/events/auth/MemberCreatedEventListener.java
+++ src/main/java/me/gg/pinit/pinittask/infrastructure/events/auth/MemberCreatedEventListener.java
@@ -5,6 +5,8 @@ import me.gg.pinit.pinittask.application.member.service.MemberService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.time.ZoneId;
+
 @Component
 public class MemberCreatedEventListener {
     private final MemberService memberService;
@@ -15,7 +17,11 @@ public class MemberCreatedEventListener {
 
     @RabbitListener(queues = AuthMemberMessaging.MEMBER_CREATED_QUEUE)
     public void on(MemberCreatedEventDto event) {
-        memberService.enrollMember(event.memberId(), event.nickname());
+        memberService.enrollMember(event.memberId(), event.nickname(), resolveZoneId());
+    }
+
+    private ZoneId resolveZoneId() {
+        return ZoneId.of("Asia/Seoul");
     }
 
 }
